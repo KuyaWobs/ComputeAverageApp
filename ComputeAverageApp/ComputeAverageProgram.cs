5446a3aad416be204dde4895d763d03ae8e0ef60
Buenaventura.298870 <Buenaventura.298870@LAB-A-PC17>
2024-02-23 07:44:40 +0800

    Add project files.

---
+++ ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static double grades, totalG, totalAve;
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                grades = Convert.ToDouble(Console.ReadLine());
+
+                totalG += grades;
+            
+            }
+
+            totalAve = totalG / 5;
+
+            Console.WriteLine("The average is " + totalAve + " and round off to " + Math.Round(totalAve));
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
